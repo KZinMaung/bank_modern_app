733654d5d32d2129a63d4efd7bc7bba4d0e58782
Kay <kayzinmg363@gmail.com>
2023-03-26 15:24:42 -0700

    business - section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({styles}) => (
+  <button type='button' className={`px-6 py-4 bg-blue-gradient rounded-[10px] outline-none text-primary text-[18px] font-poppins font-medium ${styles}`}>
+    Get Started
+  </button>
+);
 
 export default Button
